547daddabf9a42a4332b02dbb082d4d818e4ca12
Sukitai <kitaitakayoshi@gmail.com>
2020-07-04 11:09:00 +0900

    added rendering function arise from validation error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,11 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: %i[show, edit, update destroy]
+  before_action :set_post, only: %i[show edit update destroy]
 
   def index
     @posts = Post.order(id: :asc)
   end
 
   def show
-    
   end
 
   def new
@@ -14,8 +13,13 @@ class PostsController < ApplicationController
   end
 
   def create
-    post = Post.create!(post_params)
-    redirect_to post, notice: "投稿しました"
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, notice: "投稿しました"
+    else
+      flash.now[:alert] = "投稿に失敗しました"
+      render :new
+    end
   end
 
   def edit
@@ -24,8 +28,12 @@ class PostsController < ApplicationController
 
   def update
     
-    post.update!(post_params)
-    redirect_to post, notice: "投稿しました"
+    if @post.update(post_params)
+      redirect_to post, notice: "投稿しました"
+    else
+      flash.now[:alert] = "更新に失敗しました"
+      render :edit
+    end
   end
 
   def destroy
